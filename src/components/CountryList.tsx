fac783b71ad7928adfc3c489bf7565510c95d6a4
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2020-04-12 15:37:42 +0200

    be able to select country

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -7,7 +7,15 @@ export interface MyProps {
   title: string;
 }
 
-type MyState = { countries: Country[] };
+type MyState = { countries: Country[], selectedCountry: number };
+
+const liSelectedStyle = {
+    color: 'blue'
+};
+
+const liStyle = {
+    cursor: 'pointer'
+};
 
 export class CountryList extends React.Component<MyProps, MyState>
 {
@@ -15,7 +23,7 @@ export class CountryList extends React.Component<MyProps, MyState>
     {
         super(props);
 
-        this.state = {countries: []};
+        this.state = {countries: [], selectedCountry: -1};
     }
 
     componentDidMount()
@@ -23,14 +31,25 @@ export class CountryList extends React.Component<MyProps, MyState>
         API.getCountries((o: Country[]) => { this.setState({ countries: o }); });
     }
 
+    countryClick(index: number)
+    {
+        this.setState({ selectedCountry: index});
+    }
+
     render()
     {
         return (
             <div>
                 <h1>{ this.props.title }</h1>
+                <p>{this.state.selectedCountry > -1 ? (this.state.selectedCountry) : "No country selected"}</p>
                 <ul>
                 {this.state.countries.map((item, index) => (
-                    <li key={ index }>{ item['name'] } ({ item['code'] })</li>
+                    <li
+                        style={index == this.state.selectedCountry ? liSelectedStyle : liStyle}
+                        onClick={ () => this.countryClick(index) }
+                        key={ index }>
+                        { item['name'] } ({ item['code'] })
+                    </li>
                 ))}
                 </ul>
             </div>
